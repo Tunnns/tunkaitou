3d8850238e152fd40ce1e5edfe56db2ad437c2fa
Kaitouu <ismetuannguyen@gmail.com>
2025-10-22 15:15:49 +0700

    udpate abcxyz

--- bigdteam/src/js/script.js
+++ bigdteam/src/js/script.js
@@ -78,3 +78,4 @@ document.addEventListener("DOMContentLoaded", () => {
   updateYouTubeAvatar(bigd);
 });
 
+// ------------------------------------ MOBILE UI/UX -----------------------------------------
